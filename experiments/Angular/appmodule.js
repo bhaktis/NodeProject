557e13eed75cc8bb1ee9085c6e1fe2af20779190
bhaktis <sanglikar.b@husky.neu.edu>
2015-02-16 13:47:24 -0500

    angular JS experiments

---
+++ experiments/Angular/appmodule.js
@@ -0,0 +1,17 @@
+﻿var mainApp = angular.module("mainApp", []);
+
+mainApp.controller("studentController", function ($scope) {
+    $scope.student = {
+        firstname: "Nidhi",
+        lastname: "Kurup",        
+        fullname: function () {
+            var studentObject;
+            studentObject = $scope.student;
+            return studentObject.firstname + " " + studentObject.lastname;
+        },
+        reset: function () {
+            $scope.student.firstname = "Mahesh";
+            $scope.student.lastname = "Parashar";
+        }   
+    };
+});
